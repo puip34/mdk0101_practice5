79ac16787864e8de3d6fbd9b35cab11d2444230e
puip34 <123201605+puip34@users.noreply.github.com>
2023-03-24 13:08:10 +0300

    Add files via upload

---
+++ mdk0101_practice5.py
@@ -0,0 +1,38 @@
+import math
+
+
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def area(self):
+        pass
+
+    def perimeter(self):
+        pass
+
+
+class Rectangle(Shape):
+    def __init__(self, x, y, width, height):
+        super().__init__(x, y)
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        return 2 * (self.width + self.height)
+
+
+class Circle(Shape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y)
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        return 2 * math.pi * self.radius
